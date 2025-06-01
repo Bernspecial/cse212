f6311af084d66eb95e31fa72c5b76a82f3a47b1f
Bernspecial <michaelbernardolayemi@gmail.com>
2025-06-01 11:46:51 +0100

    Week 04

--- week04/code/LinkedList.cs
+++ week04/code/LinkedList.cs
@@ -33,6 +33,19 @@ public class LinkedList : IEnumerable<int>
     public void InsertTail(int value)
     {
         // TODO Problem 1
+        Node newNode = (Node)new(value);
+
+        if (_tail is null)
+        {
+            _tail = newNode;
+            _head = newNode;
+        }
+        else
+        {
+            newNode.Prev = _tail;
+            _tail.Next = newNode;
+            _tail = newNode;
+        }
     }
 
 
@@ -65,6 +78,17 @@ public class LinkedList : IEnumerable<int>
     public void RemoveTail()
     {
         // TODO Problem 2
+        if (_tail == _head)
+        {
+            _tail = null;
+            _head = null;
+        }
+
+        else if (_tail is not null)
+        {
+            _tail.Prev!.Next = null;
+            _tail = _tail.Prev;
+        }
     }
 
     /// <summary>
@@ -109,6 +133,41 @@ public class LinkedList : IEnumerable<int>
     public void Remove(int value)
     {
         // TODO Problem 3
+
+        // Empty list
+        if (_head == null) return;
+
+        // Special case: value is at head
+        if (_head.Data == value)
+        {
+            RemoveHead();
+            return;
+        }
+
+        // Search for the node to remove
+        Node? curr = _head;
+        Node? prev = null;
+
+        while (curr != null)
+        {
+            if (curr.Data == value)
+            {
+                if (curr == _tail)
+                {
+                    RemoveTail();
+                }
+                else
+                {
+                    // Skip the current node by updating the previous node's Next pointer
+                    prev!.Next = curr.Next;
+                    // Update the next node's Prev pointer to maintain doubly-linked structure
+                    curr.Next!.Prev = prev;
+                }
+                return; // Stop after removing the first match
+            }
+            prev = curr;
+            curr = curr.Next;
+        }
     }
 
     /// <summary>
@@ -117,6 +176,16 @@ public class LinkedList : IEnumerable<int>
     public void Replace(int oldValue, int newValue)
     {
         // TODO Problem 4
+        Node? curr = _head;
+
+        while (curr != null)
+        {
+            if (curr.Data == oldValue)
+            {
+                curr.Data = newValue;
+            }
+            curr = curr.Next;
+        }
     }
 
     /// <summary>
@@ -147,7 +216,14 @@ public class LinkedList : IEnumerable<int>
     public IEnumerable Reverse()
     {
         // TODO Problem 5
-        yield return 0; // replace this line with the correct yield return statement(s)
+        Node? curr = _tail;
+
+        while (curr is not null)
+        {
+            yield return curr.Data;
+            curr = curr.Prev; // replace this line with the correct yield return statement(s)
+
+        }
     }
 
     public override string ToString()
@@ -168,8 +244,10 @@ public class LinkedList : IEnumerable<int>
     }
 }
 
-public static class IntArrayExtensionMethods {
-    public static string AsString(this IEnumerable array) {
+public static class IntArrayExtensionMethods
+{
+    public static string AsString(this IEnumerable array)
+    {
         return "<IEnumerable>{" + string.Join(", ", array.Cast<int>()) + "}";
     }
 }
